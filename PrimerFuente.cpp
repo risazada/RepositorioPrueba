224c4ac596430d5f9677ebc7f61f1749e95e232c
risazada <rlisaza@elpoli.edu.co>
2015-10-26 23:09:41 -0500

    Archivo
    Revisión 1

---
+++ PrimerFuente.cpp
@@ -0,0 +1,10 @@
+#include <iostream.h>
+#include <stdlib.h>
+
+int main(){
+     system("cls");
+     cout<<"Este es un texto";
+     cout<<"Este es un texto";     
+     system("pause");
+     return 0;
+}
